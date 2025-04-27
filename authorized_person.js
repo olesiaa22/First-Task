eec031d51cac60c9713f3ddf0774db04061a264f
olesiaa22 <olesiashpotak22@gmail.com>
2025-04-27 13:14:10 +0300

    complete tabs change logic

---
+++ authorized_person.js
@@ -0,0 +1,26 @@
+export const authorizedPerson = {
+  john: {
+    surname: "Doe",
+    name: "John",
+    address: "email@example.com",
+    phone: "067 321 57 33",
+    id_number: "019283746",
+    licence_number: "019283746",
+  },
+  albert: {
+    surname: "Flores",
+    name: "Albert",
+    address: "email@example.com",
+    phone: "073 063 33 93",
+    id_number: "380645708",
+    licence_number: "380645708",
+  },
+  alice: {
+    surname: "Smith",
+    name: "Alice",
+    address: "email@example.com",
+    phone: "063 063 00 00",
+    id_number: "380000000",
+    licence_number: "380000000",
+  },
+};
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import { authorizedPerson } from "./authorized_person.js";
+
 const phoneInput = document.getElementById("phone");
 const idNumberInput = document.getElementById("id_number");
 const licenceNumberInput = document.getElementById("licence_number");
 
-//correct input for telephone
+//correct input for phone number
 phoneInput.addEventListener("input", function () {
   let digits = this.value.replace(/\D/g, "");
   let formatted = "";
@@ -23,50 +25,84 @@ function allowOnlyDigits(input) {
 allowOnlyDigits(idNumberInput);
 allowOnlyDigits(licenceNumberInput);
 
-const authorizedPerson = {
-  surname: "Doe",
-  name: "John",
-  address: "john@example.com",
-  phone: "067 321 57 33",
-  id_number: "019283746",
-  licence_number: "019283746",
-};
+// inputs utils
+function fillInputs(person) {
+  if (!person) return;
 
-function fillAuthorizedPersonInputs(person) {
-  document.getElementById("surname").value = person.surname;
-  document.getElementById("name").value = person.name;
-  document.getElementById("address").value = person.address;
-  document.getElementById("phone").value = person.phone;
-  document.getElementById("id_number").value = person.id_number;
-  document.getElementById("licence_number").value = person.licence_number;
+  document.getElementById("surname").value = person.surname ?? "";
+  document.getElementById("name").value = person.name ?? "";
+  document.getElementById("email-address").value = person.address ?? "";
+  document.getElementById("phone").value = person.phone ?? "";
+  document.getElementById("id_number").value = person.id_number ?? "";
+  document.getElementById("licence_number").value = person.licence_number ?? "";
 
   document.getElementById("full-name-user").textContent =
-    person.name + " " + person.surname;
+    person.name + " " + person.surname ?? "";
 }
 
-function openButton(event, tabName) {
-  if (event) {
-    event.preventDefault();
-  }
-
-  if (tabName === "Authorized Persons") {
-    location.hash = "#auth";
-    fillAuthorizedPersonInputs(authorizedPerson);
-  } else if (tabName === "Video Call") {
-    location.hash = "#video";
-  } else if (tabName === "User Information") {
-    location.hash = "#user";
-  }
-
-  const tabs = document.querySelectorAll(".tab");
-  tabs.forEach((tab) => tab.classList.remove("active"));
-  if (event) {
-    event.currentTarget.classList.add("active");
-  }
+function clearInputs() {
+  document.getElementById("surname").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("email-address").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("id_number").value = "";
+  document.getElementById("licence_number").value = "";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (location.hash === "#auth") {
-    fillAuthorizedPersonInputs(authorizedPerson);
-  }
+// change tab logic
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    const tabId = tab.getAttribute("id");
+
+    // fill inputs based on the clicked tab
+    if (tabId === "authorized-tab") {
+      fillInputs(authorizedPerson.albert);
+
+      // update query params
+      const url = new URL(window.location.href);
+      url.searchParams.set("user", "albert");
+      window.history.pushState(null, "", url.toString());
+    } else {
+      clearInputs();
+
+      // clear query params
+      const url = new URL(window.location.href);
+      url.searchParams.delete("user");
+      window.history.pushState(null, "", url.toString());
+    }
+
+    // update tabs styles
+    tabs.forEach((t) => {
+      if (t === tab) {
+        t.classList.add("active");
+      } else {
+        t.classList.remove("active");
+      }
+    });
+  });
 });
+
+// url params logic
+const params = new URLSearchParams(window.location.search);
+const username = params.get("user");
+const person = authorizedPerson[username];
+
+if (username && person) {
+  tabs.forEach((tab) => {
+    // make authorized tab active
+    if (tab.getAttribute("id") === "authorized-tab") {
+      tab.classList.add("active");
+
+      // update search params
+      const url = new URL(window.location.href);
+      url.searchParams.set("user", username);
+      window.history.pushState(null, "", url.toString());
+
+      // fill inputs
+      fillInputs(authorizedPerson[username]);
+    } else {
+      tab.classList.remove("active");
+    }
+  });
+}
